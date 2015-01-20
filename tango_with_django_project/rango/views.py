538bdca75d55a04b9f3dabf7017df2b0e51ef94b
Hamza Tanveer <2105272T@student.gla.ac.uk>
2015-01-20 21:02:26 +0000

    Added Image

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,7 +6,7 @@ def index(request):
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'name': "Hamza"}
+    context_dict = {'name': "STOPPPPP!!!!"}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -89,4 +87,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
